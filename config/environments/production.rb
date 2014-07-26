559d5bd9f0fbca8c6a4b96e0aa748603d487dd88
Morgan Krey <morgankrey@gmail.com>
2014-07-26 15:49:22 -0400

    ssl production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ ProfessionallyFit::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.force_ssl = true
 end
